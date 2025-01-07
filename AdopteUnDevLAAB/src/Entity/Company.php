31907b2488fdf6372927b017f9e4db1c61d1b3d0
babanks0 <charbel-stephane.bankole@etudiant.univ-rennes.fr>
2025-01-07 14:52:00 +0100

    update profil company

--- AdopteUnDevLAAB/src/Entity/Company.php
+++ AdopteUnDevLAAB/src/Entity/Company.php
@@ -4,6 +4,7 @@ namespace App\Entity;
 
 use App\Repository\CompanyRepository;
 use App\Utils\TraitClasses\EntityTimestampableTrait;
+use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: CompanyRepository::class)]
